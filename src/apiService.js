ef5633a83547405a3d8f75c077748a6be60b76be
Nikita Ustinov <65974693+NikitaUstnov@users.noreply.github.com>
2020-11-22 23:46:52 +0200

    in progress

--- src/apiService.js
+++ src/apiService.js
@@ -1,6 +1,12 @@
 const BASE_URL = "https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ"
 const DevKey = "19188081-211c1aaa4cc6c7e30b8f313cc"
 
+
+
 function fechPhotosCard(params) {
-    return fetch('${BASE_URL}')
+    return fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=animal&page=${currentPage}&per_page=12&key=${DevKey}`).then(response => {
+    return response.json();
+  }).catch(console.error())
 }
+
+export default {fechPhotosCard}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 import './styles.css';
 import cardTeplate from "./templates/card-template.hbs"
+import API from "./apiService"
+
+let currentPage = null
+
+const inputRef = document.querySelector("input")
+const loadMoreBtnRef = document.getElementById('load-btn')
+const 
+
+loadMoreBtnRef.addEventListener('click', nextPage)
+inputRef.addEventListener('input', imageSearh)
+
+function imageSearh(evt) {
+    evt.preventDefault()
+
+}
+
+function nextPage(e) {
+    e.currentPage += 1 ;
+}
+
+console.log(currentPage)
